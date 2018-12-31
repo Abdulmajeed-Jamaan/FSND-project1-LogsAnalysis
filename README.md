74e4a27abaa83b18f831fbea53e18d3bf143cbaf
Abdulmajeed Jamaan Alzahrani <azzhraanee2011@hotmail.com>
2018-12-31 23:50:50 +0300

    improvments after review
    -use try and catch when connect to the database
    -connect to the database once only
    -add shebang at thefirst line
    - update read me

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
-Project(1) Log Analysis
-student name : Abdulmajeed Jamaan Alzahrani
-        email: azzhraanee2011@hotmail.com
+# Logs Analysis - Udacity
+### Full Stack Web Development ND
+_______________________
+## About
+ This work, is a part of the Full Stack ND, explores basic concepts regarding python, relational databases, queries and views and sub queries and how to build and refine complex queries and use them to draw business conclusions from data.
 
-1.set up VM
-  - install birtual box
-  - install vagrant
-  - download the data from here https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
-  - go to directory of the file from git bash
-  - run vagrant up
-  - run vagrant ssh
-  - run the newsdata.sql file
+ the database have three tables
+   - articles
+   - authors
+   - logs
 
-2.run the python file
-  - go to project1 directory
-  - run project1.py file
-  - see the output
+ the questions on this database are :
+    1- What are the most popular three articles of all time?.
+    2- Who are the most popular article authors of all time?.
+    3- On which days did more than 1% of requests lead to errors?.
+
+## Prerequisites
+  - download and install python (https://www.python.org/downloads/).
+  - download and install virtual box (https://www.virtualbox.org/wiki/Downloads).
+  - download and install vagrant (https://www.vagrantup.com/downloads.html).
+  - download the data from here https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip .
+  - go to directory of the file using git bash.
+  - run vagrant up.
+  - run vagrant ssh.
+  - inside the vagrant VM run the newsdata.sql file.
+
+## Run the code
+  - inside the VM go to project1 directory.
+  - run project1.py file.
+  - see the output.
--- project1.py
+++ project1.py
@@ -1,46 +1,40 @@
+#!/usr/bin/env python
 # coding: utf-8
 import psycopg2
 
 DBNAME = "news"
 
 
-def question_1():
+def question_1(cursor):
     print("\n1. What are the most popular three articles of all time?\n")
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
 
-    c.execute("""select articles.title , count(*) as views from log , articles
-    where articles.slug = substring(log.path from 10) group by articles.title
-    order by views desc limit 3;""")
+    cursor.execute("""select articles.title, count(*) as views
+    from log,articles where articles.slug = substring(log.path from 10)
+    group by articles.title order by views desc limit 3;""")
 
-    posts = c.fetchall()
-    db.close()
+    posts = cursor.fetchall()
     for title, views in posts:
         print("\""+str(title)+"\""+" — "+str(views)+" views")
     print("\n")
 
 
-def question_2():
+def question_2(cursor):
     print("\n2. Who are the most popular article authors of all time?\n")
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
 
-    c.execute("""select authors.name , count(*) as views from authors,articles,log
-    where authors.id = articles.author
+    cursor.execute("""select authors.name , count(*) as views
+    from authors,articles,log where authors.id = articles.author
     and articles.slug = substring(log.path from 10)
     group by authors.name order by views desc;""")
 
-    posts = c.fetchall()
-    db.close()
+    posts = cursor.fetchall()
     for name, views in posts:
         print(str(name)+" — "+str(views)+" views")
     print("\n")
 
 
-def question_3():
+def question_3(cursor):
     print("\n3. On which days did more than 1% of requests lead to errors?\n")
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+
     subq_for_total = """(select to_char(time, 'DD Mon YYYY') as day
     , count(*) as num from log group by to_char(time, 'DD Mon YYYY')
     order by to_char(time, 'DD Mon YYYY')) as total"""
@@ -50,17 +44,32 @@ def question_3():
     group by to_char(time, 'DD Mon YYYY')
     order by to_char(time, 'DD Mon YYYY')) as error"""
 
-    c.execute("""select total.day
+    cursor.execute("""select total.day
     , trunc((cast(error.num as decimal)/total.num)*100,2) as error
-    from """+subq_for_total+""" , """+subq_for_errors+""" where total.day = error.day
+    from """+subq_for_total+""" , """+subq_for_errors+"""
+    where total.day = error.day
     and trunc((cast(error.num as decimal)/total.num)*100,2) > 1 """)
 
-    posts = c.fetchall()
-    db.close()
+    posts = cursor.fetchall()
     for day, error in posts:
         print(str(day)+" — "+str(error)+"% errors")
     print("\n")
 
-question_1()
-question_2()
-question_3()
+
+def answerQuestions():
+    try:
+        db = psycopg2.connect(database=DBNAME)
+    except psycopg2.Error as e:
+        print ("Unable to connect to the database")
+        return
+
+    cursor = db.cursor()
+
+    question_1(cursor)
+    question_2(cursor)
+    question_3(cursor)
+
+    db.close()
+
+
+answerQuestions()
